14ecc830f21a4398eff2a8ec64887bb64cf0dafe
EthanWang-1 <ethanw97@nycstudents.net>
2025-05-12 10:00:26 -0400

    completed partA.java

--- q1/partA.java
+++ q1/partA.java
@@ -1,2 +1,9 @@
-public int walkDogs(int hour){}
-  
+public int walkDogs(int hour){
+        if(company.numAvailableDogs(hour) >= maxDogs){
+            company.updateDogs(hour, maxDogs);
+            return maxDogs;
+        } else {
+            updateDogs(hour, company.numAvailableDogs(hour))
+            return company.numAvailableDogs(hour);
+        }
+    }
