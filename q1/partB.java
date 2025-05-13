c3169d316ccc8f60fd46fb8132328a2197eb1e35
EthanWang-1 <ethanw97@nycstudents.net>
2025-05-13 09:38:14 -0400

    completed partB.java

--- q1/partB.java
+++ q1/partB.java
@@ -1,1 +1,10 @@
-
+public int dogWalkShift(int startHour, int endHour){
+  int sum = 0;
+  for( int i = startHour; i <= endHour; i++){
+    if((walkDogs(i) == maxDogs) || ( i >= 9 && i <= 17 )){
+      sum += (maxDogs*3)+3;
+  } else {
+      sum += (maxDogs*3);
+  }
+  return sum;
+}
