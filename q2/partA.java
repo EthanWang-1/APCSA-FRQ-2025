5d002d62e451dca79dc38bbb5a3a2704242ac236
EthanWang-1 <ethanw97@nycstudents.net>
2025-05-13 09:54:22 -0400

    completed partA.java

--- q2/partA.java
+++ q2/partA.java
@@ -1,1 +1,28 @@
+public class SignedText{
+  private String first;
+  private String last;
+
+  public SignedText( String x, String y){
+    first = x;
+    last = y;
+  }
+
+  public String getSignature{
+    if(first.length()==0){
+      return last;
+    } else {
+      return first.substring(0,1) + "-" +last;
+    }
+  }
+
+  public String addSignature(String str){
+    if(getSignature().equal(str.substring(0,getSignature().length()))){
+      return str.substring(getSignature()-1);
+    } else if( str.indexOf(getSignature()) != -1){
+      return str + getSignature();
+    } else {
+      return str;
+    }
+  }
+}
 
