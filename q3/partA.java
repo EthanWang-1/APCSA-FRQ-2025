b159e86f18dbd252765ba4ad016ff3a769b455a7
EthanWang-1 <ethanw97@nycstudents.net>
2025-05-15 10:08:57 -0400

    Update partA.java

--- q3/partA.java
+++ q3/partA.java
@@ -1,6 +1,6 @@
 public Round(String[] names){
-  compatitorList = new ArrayList<Competitor>();
+  competitorList = new ArrayList<Competitor>();
   for(int i = 0 ;i < names.length;i++){
-    compatitorList.add(new Competitor(names[i], i + 1));
+    competitorList.add(new Competitor(names[i], i + 1));
   }
 }
