9a49359d92fdbc924222048457fc5cb67ad9f802
EthanWang-1 <ethanw97@nycstudents.net>
2025-05-15 09:39:39 -0400

    completed partA.java

--- q4/partA.java
+++ q4/partA.java
@@ -1,1 +1,7 @@
-
+public SumOrSameGame(int numRows, int numCols){
+  puzzle = new int[numRows][numCols];
+  for(int i = 0;i < numRows; i++){
+    for(int j =0; j < numCols; j++){
+      puzzle[i][j]= (int)(Math.random() * 9) + 1;
+    }
+  }
