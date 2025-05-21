b689a84ef09c6d33737d6de433bf9f51733396a0
EthanWang-1 <ethanw97@nycstudents.net>
2025-05-20 23:39:12 -0400

    Update partB.java

--- q4/partB.java
+++ q4/partB.java
@@ -1,6 +1,6 @@
 public boolean clearPair(int row, int col){
   int num = puzzle[row][col];
-  for(int i = i; i < puzzle.length; i++){
+  for(int i = 0; i < puzzle.length; i++){
     for(int j = 0; j < puzzle[0].length; j++){
       if (rows != row && cols != col){
         if((num == puzzle[i][j]) || ((puzzle[i][j] + num) == 10){
