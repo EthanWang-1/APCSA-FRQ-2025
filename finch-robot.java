bbcfb03998839168ca162f017e1c20397cd487e6
EthanWang-1 <ethanw97@nycstudents.net>
2025-06-07 23:25:04 -0400

    Update finch-robot.java

--- finch-robot.java
+++ finch-robot.java
@@ -3,36 +3,20 @@ package FinchPackage;
 public class follow {
 	public static void main(String[] args) {
 		Finch f1 = new Finch();
-		int count=0;
+		
 		while (true) {
 			int dist = f1.getDistance();
+			if(dist>10) {
+				f1.setMotors(10,10);
+			}
+			f1.stop();
 			
-			
-			System.out.println(f1.getLight("L")+","+f1.getLight("R"));
-			if(dist>20) {
-				if(f1.getLight("L")>f1.getLight("R")) {
-					f1.setMotors(10,-10);
-				}else if(f1.getLight("L")<=f1.getLight("R")) {
-					f1.setMotors(-10, 10);
-				}
-				count++;
-				//System.out.println(dist);
+			if((int)Math.random()*2 == 1){
+				f1.turn("R",90,10);
 			}else {
-				if(dist>5) {
-					f1.setMotors(10,10);
-				}else {
-					f1.stop();
-					f1.playNote(60,0.5);
-					
-				}
+				f1.turn("R",90,10);	
 			}
-			
-			
-			if(f1.getButton("A")) {
-				break;
-			}
+			f1.stop();
 		}
-		f1.stop();
-		f1.disconnect();
-		}
+	}
 }
